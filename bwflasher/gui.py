60309d1157222ba4bc63c30ef3a0d90a72de7a5a
Amar <101881857+amar210d@users.noreply.github.com>
2025-09-27 17:07:08 +0200

    Update gui.py

--- bwflasher/gui.py
+++ bwflasher/gui.py
@@ -46,7 +46,7 @@ OS = platform.system()
 
 def resource_path(relative_path):
     if hasattr(sys, "_MEIPASS"):
-        base_path = sys._MEIPASS  # Temp-Ordner der EXE
+        base_path = sys._MEIPASS 
     else:
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
